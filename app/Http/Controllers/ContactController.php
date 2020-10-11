18e12ad9826a8797742f61eb4965f58a257ef290
Samad Guliev <samadguliev@mail.ru>
2020-10-12 01:44:12 +0300

    Migration for table, adding messages from form to database

--- app/Http/Controllers/ContactController.php
+++ app/Http/Controllers/ContactController.php
@@ -5,17 +5,22 @@ namespace App\Http\Controllers;
 use Illuminate\Http\Request;
 use App\Http\Requests\ContactRequest;
 use Illuminate\Foundation\Validation\ValidatesRequests;
+use App\Models\Contact;
 
 class ContactController extends Controller
 {
     use ValidatesRequests;
     public function submit(ContactRequest $request)
     {
-//        dd($request->input('subject'));
-//        $validation = $this->validate($request, [
-//            'subject' => 'required|min:5|max:12',
-//            'message' => 'required|min:15|max:500',
-//        ]);
+        $contact = new Contact();
+        $contact->name = $request->input('name');
+        $contact->email = $request->input('email');
+        $contact->subject = $request->input('subject');
+        $contact->message = $request->input('message');
+
+        $contact->save();
+
+        return redirect()->route('home')->with('success', 'Сообщение было добавлено');
     }
 
 }
--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -16,6 +16,7 @@
         @include('inc.hero')
     @endif
     <div class="container mt-5 ">
+        @include('inc.messages')
         <div class="row">
             <div class="col-8">
                 @yield('content')
